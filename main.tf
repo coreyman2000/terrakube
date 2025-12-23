825246f9c26115b39482db890100ba857c984d9c
coreyman2000 <corey.pooleman@gmail.com>
2025-12-23 12:18:06 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,22 +14,6 @@ provider "proxmox" {
   tmp_dir  = "/tmp"
 }
 
-# NEW: Snippet to fix Rocky Linux DNF conflict before upgrade
-resource "proxmox_virtual_environment_file" "rocky_fix" {
-  content_type = "snippets"
-  datastore_id = "local"
-  node_name    = "proxmox2"
-
-  source_raw {
-    data = <<EOF
-#cloud-config
-runcmd:
-  - [ sh, -c, "if command -v dnf > /dev/null; then dnf remove -y python-unversioned-command && dnf upgrade -y; fi" ]
-EOF
-    file_name = "rocky-fix.yaml"
-  }
-}
-
 # --- 1. DEFINE AVAILABLE IMAGES ---
 variable "cloud_images" {
   description = "Map of cloud images to download (Name -> URL)"
@@ -37,6 +21,8 @@ variable "cloud_images" {
     url       = string
     file_name = string
   }))
+  # We set defaults here so you don't HAVE to put this in Terrakube variables, 
+  # but you can override it if you want.
   default = {
     "ubuntu-jammy" = {
       url       = "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img"
@@ -49,14 +35,14 @@ variable "cloud_images" {
   }
 }
 
-# --- 2. DEFINE VMS ---
+# --- 2. DEFINE VMS (Now with 'image' selection) ---
 variable "virtual_machines" {
   description = "Map of VMs to deploy"
   type = map(object({
     cores    = number
     memory   = number
-    image    = string 
-    username = string 
+    image    = string # Must match a key in 'cloud_images' (e.g. "ubuntu-jammy")
+    username = string # Cloud-init user varies by OS (ubuntu vs debian)
   }))
 }
 
@@ -85,18 +71,16 @@ resource "proxmox_virtual_environment_vm" "vm_loop" {
   memory { dedicated = each.value.memory }
   cpu { 
     cores = each.value.cores 
-    type  = "host"
+    type  = "host"   # This replaces the default 'qemu64'
   }
 
   initialization {
-    # LINKED FIX HERE:
-    user_data_file_id = proxmox_virtual_environment_file.rocky_fix.id
-
     ip_config {
       ipv4 { address = "dhcp" }
     }
 
     user_account {
+      # Use the specific username for this VM (e.g. ubuntu or debian)
       username = each.value.username 
       password = "password"
       keys     = [var.ssh_public_key]
@@ -105,7 +89,11 @@ resource "proxmox_virtual_environment_vm" "vm_loop" {
 
   disk {
     datastore_id = "local-lvm"
+    
+    # MAGIC HAPPENS HERE:
+    # Look up the correct image ID based on the 'image' name provided in the variable
     import_from  = proxmox_virtual_environment_download_file.images[each.value.image].id
+    
     interface    = "virtio0"
     iothread     = true
     discard      = "on"
