bfb94b309cbd75d616f2f289506d713a477ad3c6
coreyman2000 <corey.pooleman@gmail.com>
2025-12-23 10:42:40 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     proxmox = {
       source = "bpg/proxmox"
-      version = "0.89.1" # CHANGED: Updated from 0.69.1 to latest
+      version = "0.69.1"
     }
   }
 }
@@ -10,16 +10,21 @@ terraform {
 provider "proxmox" {
   endpoint = var.pm_api_url
   
-  # bpg provider uses just "api_token", not "pm_api_token_..."
+  # Connect using your existing variables
+  # Format combines them: user@realm!token=secret
   api_token = "${var.pm_user}=${var.pm_password}"
   
   insecure = true
+  
+  # This provider needs a tmp folder
   tmp_dir  = "/tmp"
 }
 
 resource "proxmox_virtual_environment_vm" "ubuntu_vm" {
   name      = "test-ubuntu"
   node_name = "proxmox2"
+
+  # should be true if qemu agent is not installed / enabled on the VM
   stop_on_destroy = true
   
   agent {
@@ -27,6 +32,7 @@ resource "proxmox_virtual_environment_vm" "ubuntu_vm" {
   }
 
   initialization {
+    # THIS IS THE NEW PART FOR DHCP
     ip_config {
       ipv4 {
         address = "dhcp"
@@ -34,8 +40,9 @@ resource "proxmox_virtual_environment_vm" "ubuntu_vm" {
     }
 
     user_account {
-      username = "ubuntu" 
-      keys = [var.ssh_public_key] 
+      # do not use this in production, configure your own ssh key instead!
+      username = "user"
+      keys = [var.ssh_public_key]
     }
   }
 
@@ -63,7 +70,8 @@ resource "proxmox_virtual_environment_download_file" "ubuntu_cloud_image" {
   datastore_id = "local"
   node_name    = "proxmox2"
   url          = "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img"
-  file_name    = "jammy-server-cloudimg-amd64.qcow2"
+  # need to rename the file to *.qcow2 to indicate the actual file format for import
+  file_name = "jammy-server-cloudimg-amd64.qcow2"
 }
 
 variable "pm_api_url" { type = string }
