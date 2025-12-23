e8a384f009d983dbdb34d3ba058455b0009ad170
coreyman2000 <corey.pooleman@gmail.com>
2025-12-23 10:05:19 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,42 +20,39 @@ provider "proxmox" {
   tmp_dir  = "/tmp"
 }
 
-resource "proxmox_virtual_environment_vm" "test_server" {
-  name      = "terrakube-vm"
-  node_name = "proxmox2" # CHANGE THIS if your node is not named 'pve'
+resource "proxmox_virtual_environment_vm" "ubuntu_vm" {
+  name      = "test-ubuntu"
+  node_name = "proxmox2"
 
-  cpu {
-    cores = 2
-  }
+  # should be true if qemu agent is not installed / enabled on the VM
+  stop_on_destroy = true
 
-  memory {
-    dedicated = 2048
-  }
-
-  agent {
-    enabled = true
-  }
-
-  network_device {
-    bridge = "vmbr0"
+  initialization {
+    user_account {
+      # do not use this in production, configure your own ssh key instead!
+      username = "user"
+      password = "password"
+    }
   }
 
   disk {
-    datastore_id = "local-lvm" # CHANGE THIS if your storage is local-zfs
-    interface    = "scsi0"
-    size         = 10
-    file_format  = "raw"
-  }
-
-  cdrom {
-    file_id = "local:iso/ubuntu-20.04.iso" # Ensure this path is 100% correct in Proxmox
-  }
-  
-  operating_system {
-    type = "l26" # Linux 2.6+
+    datastore_id = "local-lvm"
+    import_from  = proxmox_virtual_environment_download_file.ubuntu_cloud_image.id
+    interface    = "virtio0"
+    iothread     = true
+    discard      = "on"
+    size         = 20
   }
 }
 
+resource "proxmox_virtual_environment_download_file" "ubuntu_cloud_image" {
+  content_type = "import"
+  datastore_id = "local"
+  node_name    = "pve"
+  url          = "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img"
+  # need to rename the file to *.qcow2 to indicate the actual file format for import
+  file_name = "jammy-server-cloudimg-amd64.qcow2"
+}
 variable "pm_api_url" { type = string }
 variable "pm_user" { type = string }
 variable "pm_password" { type = string }
