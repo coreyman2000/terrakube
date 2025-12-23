ccb69e3be95ab3984b1c1fc0fc123d7438875ff6
coreyman2000 <corey.pooleman@gmail.com>
2025-12-23 12:34:01 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
 terraform {
   required_providers {
     proxmox = {
-      source = "bpg/proxmox"
+      source  = "bpg/proxmox"
       version = "0.89.1"
     }
   }
 }
 
 provider "proxmox" {
-  endpoint = var.pm_api_url
+  endpoint  = var.pm_api_url
   api_token = "${var.pm_user}=${var.pm_password}"
-  insecure = true
-  tmp_dir  = "/tmp"
+  insecure  = true
+  tmp_dir   = "/tmp"
 }
 
 # --- 1. DEFINE AVAILABLE IMAGES ---
@@ -21,8 +21,6 @@ variable "cloud_images" {
     url       = string
     file_name = string
   }))
-  # We set defaults here so you don't HAVE to put this in Terrakube variables, 
-  # but you can override it if you want.
   default = {
     "ubuntu-jammy" = {
       url       = "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img"
@@ -35,14 +33,14 @@ variable "cloud_images" {
   }
 }
 
-# --- 2. DEFINE VMS (Now with 'image' selection) ---
+# --- 2. DEFINE VMS ---
 variable "virtual_machines" {
   description = "Map of VMs to deploy"
   type = map(object({
     cores    = number
     memory   = number
-    image    = string # Must match a key in 'cloud_images' (e.g. "ubuntu-jammy")
-    username = string # Cloud-init user varies by OS (ubuntu vs debian)
+    image    = string
+    username = string
   }))
 }
 
@@ -58,7 +56,27 @@ resource "proxmox_virtual_environment_download_file" "images" {
   overwrite_unmanaged = true
 }
 
-# --- 4. CREATE VMS ---
+# --- 4. CLOUD-CONFIG FILE ---
+# This creates the vendor configuration file on the Proxmox storage
+resource "proxmox_virtual_environment_file" "cloud_config" {
+  content_type = "snippets"
+  datastore_id = "local"
+  node_name    = "proxmox2"
+
+  source_raw {
+    data = <<EOF
+#cloud-config
+packages:
+  - qemu-guest-agent
+runcmd:
+  - systemctl enable qemu-guest-agent
+  - systemctl start qemu-guest-agent
+EOF
+    file_name = "guest-agent-config.yaml"
+  }
+}
+
+# --- 5. CREATE VMS ---
 resource "proxmox_virtual_environment_vm" "vm_loop" {
   for_each = var.virtual_machines
 
@@ -66,21 +84,24 @@ resource "proxmox_virtual_environment_vm" "vm_loop" {
   node_name       = "proxmox2"
   stop_on_destroy = true
   
-  agent { enabled = false }
+  # Set to true now that we are installing the agent
+  agent { enabled = true }
 
   memory { dedicated = each.value.memory }
   cpu { 
     cores = each.value.cores 
-    type  = "host"   # This replaces the default 'qemu64'
+    type  = "host"
   }
 
   initialization {
+    # Reference the snippet created above
+    user_data_file_id = proxmox_virtual_environment_file.cloud_config.id
+
     ip_config {
       ipv4 { address = "dhcp" }
     }
 
     user_account {
-      # Use the specific username for this VM (e.g. ubuntu or debian)
       username = each.value.username 
       password = "password"
       keys     = [var.ssh_public_key]
@@ -89,11 +110,7 @@ resource "proxmox_virtual_environment_vm" "vm_loop" {
 
   disk {
     datastore_id = "local-lvm"
-    
-    # MAGIC HAPPENS HERE:
-    # Look up the correct image ID based on the 'image' name provided in the variable
     import_from  = proxmox_virtual_environment_download_file.images[each.value.image].id
-    
     interface    = "virtio0"
     iothread     = true
     discard      = "on"
