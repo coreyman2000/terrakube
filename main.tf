0138a83f0e8840c868c44fbf676d0fcaf67770d4
coreyman2000 <corey.pooleman@gmail.com>
2025-12-23 15:41:32 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -61,6 +61,7 @@ resource "proxmox_virtual_environment_file" "cloud_config" {
 #cloud-config
 # 1. Update and install Guest Agent
 package_update: true
+package_upgrade: true
 packages:
   - qemu-guest-agent
   - [python-unversioned-command, false]
