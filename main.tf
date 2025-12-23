e7de6c58ccede07c20e7d5967f5d865b6d2541a7
coreyman2000 <corey.pooleman@gmail.com>
2025-12-23 11:36:56 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,53 +14,71 @@ provider "proxmox" {
   tmp_dir  = "/tmp"
 }
 
-# --- 1. NEW VARIABLE FOR MULTIPLE VMS ---
+# --- 1. DEFINE AVAILABLE IMAGES ---
+variable "cloud_images" {
+  description = "Map of cloud images to download (Name -> URL)"
+  type = map(object({
+    url       = string
+    file_name = string
+  }))
+  # We set defaults here so you don't HAVE to put this in Terrakube variables, 
+  # but you can override it if you want.
+  default = {
+    "ubuntu-jammy" = {
+      url       = "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img"
+      file_name = "jammy-server-cloudimg-amd64.qcow2"
+    }
+    "debian-12" = {
+      url       = "https://cloud.debian.org/images/cloud/bookworm/latest/debian-12-generic-amd64.qcow2"
+      file_name = "debian-12-generic-amd64.qcow2"
+    }
+  }
+}
+
+# --- 2. DEFINE VMS (Now with 'image' selection) ---
 variable "virtual_machines" {
   description = "Map of VMs to deploy"
-  # This expects a format like:
-  # {
-  #   "vm-name-1" = { cores = 2, memory = 2048 }
-  #   "vm-name-2" = { cores = 4, memory = 4096 }
-  # }
   type = map(object({
-    cores  = number
-    memory = number
+    cores    = number
+    memory   = number
+    image    = string # Must match a key in 'cloud_images' (e.g. "ubuntu-jammy")
+    username = string # Cloud-init user varies by OS (ubuntu vs debian)
   }))
 }
 
-resource "proxmox_virtual_environment_vm" "ubuntu_vm" {
-  # --- 2. THE LOOP ---
+# --- 3. DOWNLOAD ALL IMAGES ---
+resource "proxmox_virtual_environment_download_file" "images" {
+  for_each = var.cloud_images
+
+  content_type        = "import"
+  datastore_id        = "local"
+  node_name           = "proxmox2"
+  url                 = each.value.url
+  file_name           = each.value.file_name
+  overwrite_unmanaged = true
+}
+
+# --- 4. CREATE VMS ---
+resource "proxmox_virtual_environment_vm" "vm_loop" {
   for_each = var.virtual_machines
 
-  # Use the key from the variable (e.g., "web-server") as the VM Name
-  name      = each.key
-  node_name = "proxmox2"
+  name            = each.key
+  node_name       = "proxmox2"
   stop_on_destroy = true
   
-  agent {
-    enabled = false
-  }
+  agent { enabled = false }
 
-  # --- 3. DYNAMIC SPECS ---
-  memory {
-    # Use the value from the variable (e.g., 2048)
-    dedicated = each.value.memory
-  }
-
-  cpu {
-    # Use the value from the variable (e.g., 2)
-    cores = each.value.cores
-  }
+  memory { dedicated = each.value.memory }
+  cpu    { cores = each.value.cores }
 
   initialization {
     ip_config {
-      ipv4 {
-        address = "dhcp"
-      }
+      ipv4 { address = "dhcp" }
     }
 
     user_account {
-      username = "user"
+      # Use the specific username for this VM (e.g. ubuntu or debian)
+      username = each.value.username 
       password = "password"
       keys     = [var.ssh_public_key]
     }
@@ -68,7 +86,11 @@ resource "proxmox_virtual_environment_vm" "ubuntu_vm" {
 
   disk {
     datastore_id = "local-lvm"
-    import_from  = proxmox_virtual_environment_download_file.ubuntu_cloud_image.id
+    
+    # MAGIC HAPPENS HERE:
+    # Look up the correct image ID based on the 'image' name provided in the variable
+    import_from  = proxmox_virtual_environment_download_file.images[each.value.image].id
+    
     interface    = "virtio0"
     iothread     = true
     discard      = "on"
@@ -76,22 +98,8 @@ resource "proxmox_virtual_environment_vm" "ubuntu_vm" {
     file_format  = "raw"
   }
   
-  network_device {
-    bridge = "vmbr0"
-  }
-  
-  operating_system {
-    type = "l26"
-  }
-}
-
-resource "proxmox_virtual_environment_download_file" "ubuntu_cloud_image" {
-  content_type = "import"
-  datastore_id = "local"
-  node_name    = "proxmox2"
-  url          = "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img"
-  file_name    = "jammy-server-cloudimg-amd64.qcow2"
-  overwrite_unmanaged = true
+  network_device { bridge = "vmbr0" }
+  operating_system { type = "l26" }
 }
 
 variable "pm_api_url" { type = string }
