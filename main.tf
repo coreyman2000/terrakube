af1b86dbf4e168515ed043e13cc11a9d3a9b16ad
coreyman2000 <corey.pooleman@gmail.com>
2026-03-11 10:09:03 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -84,7 +84,7 @@ resource "proxmox_virtual_environment_download_file" "images" {
   for_each = var.cloud_images
 
   content_type        = "import"
-  datastore_id        = "ceph-pool1"
+  datastore_id        = "nfs-iso"
   node_name           = "csc-pve-node-1"
   url                 = each.value.url
   file_name           = each.value.file_name
